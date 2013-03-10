758e7a4b3903ade2887292bc0ea4f8f986a6896b
Taavi Ilp <taaviilp@taaviilp-VirtualBox.(none)>
2013-03-10 16:00:56 +0200

    db test startupiscripti (töötab isegi)

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 if (Meteor.isServer) {
   Meteor.startup(function () {
     console.log("The Application has started...");
+    //väike DBtest
+    //Test = new Meteor.Collection("election2013");
+    //var tmp = Test.findOne({id: 1});
+    //console.log(tmp.name);
   });
 }
