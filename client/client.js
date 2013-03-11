d51db2971f73c0dbf61b04f43e16446961d4dd4c
Taavi Ilp <taaviilp@taaviilp-VirtualBox.(none)>
2013-03-11 23:28:56 +0200

    parsib kandidaadi_info querystringi lahti

--- client/client.js
+++ client/client.js
@@ -2,16 +2,40 @@ if (Meteor.isClient) {
 
   ElectionData = new Meteor.Collection("election2013"); //2013 valimiste andmed  
 
+
   Meteor.startup(function () {
      Meteor.subscribe("election2013"); //sünkime serveriga   
   });
-
 
     var myAppRouter = Backbone.Router.extend({
         routes: {
             "*path": "main"
         },
-        main: function(url_path){
+        main: function(url_path){            
+            var urlElements = url_path.split("?");
+             
+            if (urlElements.length > 1) {
+
+	            //parseme lahti
+                    var parameters = {};
+                    var parameterPairArray = urlElements[1].split("&");
+                    for (var i = 0; i < parameterPairArray.length; i++) {
+			var pair = parameterPairArray[i].split("=");
+                        console.log(pair);
+			parameters[pair[0]] = pair[1];
+		    }
+
+
+		    if (urlElements[0] == "kandidaadi_info") {                       
+			//TODO - siin teha check, et üldse "id" parameeter leiduks
+			//MIDA TEHA SIIS, KUI SEDA POLE?
+                    	Session.set("candidate_info_id", parameters["id"]);		    
+	            };
+
+		//see jätta viimaseks reaks, et basepathi normaalselt saaks..
+		url_path = urlElements[0];
+	    };            
+
             Session.set('page_id', url_path);
         }
     });
@@ -62,38 +86,41 @@ if (Meteor.isClient) {
     };
 
     Template.tulemused.regions = function () {
-	var regions = getRegions();
+        var regions = getRegions();
+	if (regions) {
 
-        var ret = [];
+		var ret = [];
 
-        for (var i = 0; i < regions.length; i++) {
-		var tmpReg = {};
-                tmpReg.name = regions[i].name;
-                tmpReg.cid = regions[i].cid;
-                ret.push(tmpReg);
-	};
-   
-        return ret;
+		for (var i = 0; i < regions.length; i++) {
+			var tmpReg = {};
+		        tmpReg.name = regions[i].name;
+		        tmpReg.cid = regions[i].cid;
+		        ret.push(tmpReg);
+		};
+	   
+		return ret;
+	}
     };
 
 
 //TODO - panna sõltuma filtreerimisvalikutest..
     Template.tulemused.candidates = function () {
-	var persons = getPersons();
+        var persons = getPersons();	
+        if (persons) {
+		var ret = [];
 
-        var ret = [];
-
-        for (var i = 0; i < persons.length; i++) {
-		var tmpPer = {};
-                tmpPer.firstName = persons[i].firstName;
-                tmpPer.lastName = persons[i].lastName;
-                tmpPer.regionName = getRegionNameById(persons[i].regionId);
-                tmpPer.partyName = getPartyNameById(persons[i].partyId);
-                tmpPer.votes = 0; //TODO!
-                ret.push(tmpPer);
-	};
-   
-        return ret;
+		for (var i = 0; i < persons.length; i++) {
+			var tmpPer = {};
+		        tmpPer.firstName = persons[i].firstName;
+		        tmpPer.lastName = persons[i].lastName;
+		        tmpPer.regionName = getRegionNameById(persons[i].regionId);
+		        tmpPer.partyName = getPartyNameById(persons[i].partyId);
+		        tmpPer.votes = 0; //TODO!
+		        ret.push(tmpPer);
+		};
+	   
+		return ret;
+	}
     };
 
     Template.nimekirjad.candidates = function () {
@@ -101,49 +128,92 @@ if (Meteor.isClient) {
     };
 
     Template.kandidaadi_info.candidate = function () {   
-         
-       var candidate = getPersons().filter(function(el) {
-       	   return el.cid === 1; //TODO MUUTA QUERYSTRING ABIL VÕI SESSION MAP ABIL VMS!!
-       });
-      
-       var person = {};
-       person.firstName = candidate[0].firstName;
-       person.lastName = candidate[0].lastName;
-       person.regionName = getRegionNameById(candidate[0].regionId);
-       person.partyName = getPartyNameById(candidate[0].partyId);
-              
-       if (candidate[0].partyId != 0) {
-       	person.listPosition = candidate[0].listPosition;
-       }
-       else {
-       	person.listPosition = "-"; //TODO - #unless abil üldse üksikkandidaatide puhul mitte näidata?
-       };
-       person.registrationNr = candidate[0].registrationNr;
-       person.maritalStatus = "TODO";
-       person.phone = candidate[0].phone;
-       person.email = candidate[0].email;
-       person.description = candidate[0].description;
+       
 
-       return person;
+       var persons = getPersons();
+
+       if (persons) {
+		 
+	       var candidate = persons.filter(function(el) {
+	       	   return el.cid == Session.get("candidate_info_id");
+	       });
+		//TODO - siia kontroll, kas üldse keegi leiti!
+	        //KUI EI, NÄIDATA MINGIT LEHTE ET "SELLIST KANDIDAATI EI LEIDU"
+	      
+	       var person = {};
+	       person.firstName = candidate[0].firstName;
+	       person.lastName = candidate[0].lastName;
+	       person.regionName = getRegionNameById(candidate[0].regionId);
+	       person.partyName = getPartyNameById(candidate[0].partyId);
+		      
+	       if (candidate[0].partyId != 0) {
+	       	person.listPosition = candidate[0].listPosition;
+	       }
+	       else {
+	       	person.listPosition = "-"; //TODO - #unless abil üldse üksikkandidaatide puhul mitte 	
+					   //näidata?
+	       };
+	       person.registrationNr = candidate[0].registrationNr;
+	       person.maritalStatus = "TODO";
+	       person.phone = candidate[0].phone;
+	       person.email = candidate[0].email;
+	       person.description = candidate[0].description;
+
+	       return person;
+	}
     };
 
 
     
     //andmebaasist arrayde saamise funktsioonid
     getPersons = function() {
-    	var tmp = ElectionData.findOne();
-    	return (tmp && tmp.persons);
+    	var data = ElectionData.findOne();
+    	if (data) {
+		return data.persons;
+		/*
+	        var regions = [];
+		for (var i = 0; i < data.regions.length; i++) {			
+			var region = {};
+                        region.name = data.regions[i].name;
+			region.cid = data.regions[i].cid;
+                        regions.push(region);
+		};
+		return regions;
+		*/
+	};
     };
 
 
     getRegions = function() {
-    	var tmp = ElectionData.findOne();
-    	return (tmp && tmp.regions);
+    	var data = ElectionData.findOne();
+    	if (data) {
+	        var regions = [];
+		for (var i = 0; i < data.regions.length; i++) {			
+			var region = {};
+                        region.name = data.regions[i].name;
+			region.cid = data.regions[i].cid;
+                        regions.push(region);
+		};
+		return regions;
+	};
     };
 
     getParties = function() {
-    	var tmp = ElectionData.findOne();
-    	return (tmp && tmp.parties);
+    	var data = ElectionData.findOne();
+    	if (data) {
+		return data.parties;
+
+/*
+	        var regions = [];
+		for (var i = 0; i < data.regions.length; i++) {			
+			var region = {};
+                        region.name = data.regions[i].name;
+			region.cid = data.regions[i].cid;
+                        regions.push(region);
+		};
+		return regions;
+*/
+	};
     };
 
 
--- server/server.js
+++ server/server.js
@@ -1,12 +1,10 @@
 if (Meteor.isServer) {
 
+  Meteor.startup(function () {
   ElectionData = new Meteor.Collection("election2013"); //2013 andmed
-
   Meteor.publish("election2013", function() {	
 	return ElectionData.find({cid : 1});
   });
-
-  Meteor.startup(function () {
     console.log("The Application has started...");
   });
 }
