24ebd2f102d905a967bdbd3f74c6997e490d2ff5
Taavi Jaanson <regdyn@gmail.com>
2013-03-12 09:43:45 +0200

    Form validation for "kandidaadi_vorm"

---
+++ client/validate.js
@@ -0,0 +1,59 @@
+﻿function validate() {
+  if(!document.getElementById) return;
+
+  // get form variables
+  var phone = document.getElementById("inputPhone").value;
+  var email = document.getElementById("inputEmail").value;
+  var desc = document.getElementById("inputDescription").value;
+  var incorrect = new Array();
+  var no = 0;
+  var regExp = /[A-Za-z]{2,6}/;
+
+  //var pluspos=phone.indexOf("+");
+  if(phone.length < 7 ) {
+   	incorrect[no] = "1";
+   	no++;
+   	phone = "";
+  } 
+  /*
+  else if ((pluspos != 0 && (phone.length < 11 || phone.length > 12) )) {
+   	incorrect[no] = "1";
+   	no++;
+   	phone = "";    
+  }
+  */
+
+  var atpos=email.indexOf("@");
+  var dotpos=email.lastIndexOf(".");
+  if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length || email.length < 6) {
+    incorrect[no] = "2";
+    no++;
+    email = "";
+  }  
+  
+  if(regExp.test(desc)) {
+  } else {
+   	incorrect[no] = "3";
+   	no++;
+   	desc = "";
+  }
+ 
+  
+  for(j=0;j<no;j++) {
+    document.getElementById(incorrect[j]).style.color="#FF0000";
+  }
+
+  if(no > 0) {
+   	document.getElementById("errors").innerHTML = "<span class=\"error\">Viga vormi täitsmisel. Palun täitkse kõik nõutud väljad.</span><br />";
+  }
+  else {
+    for(i=1;i<4;i++) {
+      document.getElementById(i).style.color="#333333";
+    }
+  }
+
+  document.getElementById("inputPhone").value = phone;
+  document.getElementById("inputEmail").value = email;
+  document.getElementById("inputDescription").value = desc;
+}
+
