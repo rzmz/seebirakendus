ebf2b386cbf61af78d4ce77a3b74851d75fa7678
Taavi Jaanson <regdyn@gmail.com>
2013-03-12 10:06:17 +0200

    My bad, had a bug there...

--- client/validate.js
+++ client/validate.js
@@ -1,14 +1,12 @@
 ﻿function validate() {
   if(!document.getElementById) return;
-
+    
   // get form variables
   var phone = document.getElementById("inputPhone").value;
   var email = document.getElementById("inputEmail").value;
   var desc = document.getElementById("inputDescription").value;
   var incorrect = new Array();
   var no = 0;
-  var regExp = /[A-Za-z]{2,6}/;
-
   //var pluspos=phone.indexOf("+");
   if(phone.length < 7 ) {
    	incorrect[no] = "1";
@@ -25,19 +23,20 @@
 
   var atpos=email.indexOf("@");
   var dotpos=email.lastIndexOf(".");
-  if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length || email.length < 6) {
+  if (atpos<1 || dotpos<atpos+2 || dotpos+2>=email.length || email.length < 6) {
     incorrect[no] = "2";
     no++;
     email = "";
   }  
   
-  if(regExp.test(desc)) {
-  } else {
+  if(desc.length < 3) {
    	incorrect[no] = "3";
    	no++;
    	desc = "";
   }
- 
+   for(i=1;i<4;i++) {
+    document.getElementById(i).style.color="#333333";
+  }
   
   for(j=0;j<no;j++) {
     document.getElementById(incorrect[j]).style.color="#FF0000";
@@ -47,11 +46,8 @@
    	document.getElementById("errors").innerHTML = "<span class=\"error\">Viga vormi täitsmisel. Palun täitkse kõik nõutud väljad.</span><br />";
   }
   else {
-    for(i=1;i<4;i++) {
-      document.getElementById(i).style.color="#333333";
-    }
+    document.getElementById("errors").innerHTML = "";
   }
-
   document.getElementById("inputPhone").value = phone;
   document.getElementById("inputEmail").value = email;
   document.getElementById("inputDescription").value = desc;
