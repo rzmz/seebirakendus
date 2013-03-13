6b352731eb447cbb908f2ae11eae42049f930e2e
Taavi Ilp <taaviilp@taaviilp-VirtualBox.(none)>
2013-03-13 09:42:13 +0200

    mässime tulemuste lehe templatede elemendid ka ilusti sisse

--- client/results.js
+++ client/results.js
@@ -15,46 +15,52 @@ Template.tulemused.regions = function () {
 
 //TODO - panna sõltuma filtreerimisvalikutest..
 Template.tulemused.candidates = function () {
-    //TODO - teha mingi ühine meetod kandidaadiinfo saamiseks
-    var persons = getPersons();
-    if (persons) {
-        var ret = [];
 
-        for (var i = 0; i < persons.length; i++) {
-            var tmpPer = {};
-            tmpPer.cid = persons[i].cid;
-            tmpPer.firstName = persons[i].firstName;
-            tmpPer.lastName = persons[i].lastName;
-            tmpPer.regionName = getRegionNameById(persons[i].regionId);
-            tmpPer.partyId = persons[i].partyId;
-            if (persons[i].partyId == 0) tmpPer.noPartyLink = true; 
-            tmpPer.partyName = getPartyNameById(persons[i].partyId);
-		    tmpPer.votes = persons[i].votes; 
-		    ret.push(tmpPer);
-		};
-        
-		return ret;
+    if (partiesReady && regionsReady && personsReady) {
+	    //TODO - teha mingi ühine meetod kandidaadiinfo saamiseks
+	    var persons = getPersons();
+	    if (persons) {
+		var ret = [];
+
+		for (var i = 0; i < persons.length; i++) {
+		    var tmpPer = {};
+		    tmpPer.cid = persons[i].cid;
+		    tmpPer.firstName = persons[i].firstName;
+		    tmpPer.lastName = persons[i].lastName;
+		    tmpPer.regionName = getRegionNameById(persons[i].regionId);
+		    tmpPer.partyId = persons[i].partyId;
+		    if (persons[i].partyId == 0) tmpPer.noPartyLink = true; 
+		    tmpPer.partyName = getPartyNameById(persons[i].partyId);
+			    tmpPer.votes = persons[i].votes; 
+			    ret.push(tmpPer);
+			};
+		
+			return ret;
+	    }
     }
 };
 
 //TODO - panna sõltuma filtreerimisvalikutest..
 Template.tulemused.parties = function () {
     //TODO - teha mingi ühine meetod kandidaadiinfo saamiseks
-    var parties = getParties();	
-    if (parties) {
-        var ret = [];
 
-        for (var i = 0; i < parties.length; i++) {
-            var tmpPar = {};
-            tmpPar.cid = parties[i].cid;
-		    tmpPar.name = parties[i].name;
-            tmpPar.votes = getVotesByPartyId(parties[i].cid);
-            tmpPar.mandates = getMandatesByPartyId(parties[i].cid);
-            ret.push(tmpPar);
-		};
-        	   
-		return ret;
-	}
+    if (partiesReady && regionsReady && personsReady) {
+	    var parties = getParties();	
+	    if (parties) {
+		var ret = [];
+
+		for (var i = 0; i < parties.length; i++) {
+		    var tmpPar = {};
+		    tmpPar.cid = parties[i].cid;
+			    tmpPar.name = parties[i].name;
+		    tmpPar.votes = getVotesByPartyId(parties[i].cid);
+		    tmpPar.mandates = getMandatesByPartyId(parties[i].cid);
+		    ret.push(tmpPar);
+			};
+			   
+			return ret;
+		}
+     }
 };
 
 Template.tulemused.rendered = function() {
