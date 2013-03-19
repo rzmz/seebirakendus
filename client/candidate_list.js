1f48cff72843074cbc690e4a48a6ec6b86b2607b
Taavi Ilp <taaviilp@taaviilp-VirtualBox.(none)>
2013-03-20 01:07:04 +0200

    sorteerimine töötab, checkboxid kõik ka.

--- client/candidate_list.js
+++ client/candidate_list.js
@@ -1,40 +1,84 @@
 Session.set("nimekirjad_current_search_first_name_enabled", true);
+Session.set("nimekirjad_current_search_last_name_enabled", true);
 
-Template.nimekirjad.listLoading = function() {
-	return Session.get("candidates_list_loading");
-};
-
-Template.nimekirjad.candidates = function() {
+Template.nimekirjad.candidates = function() { 
 	//hack me baby one more time	
 
-	if (personsReady && regionsReady && partiesReady) {
+	if (personsReady && regionsReady && partiesReady) { 
 	     
     	    var persons = Persons.find({}).fetch();
 
 	    nimekirjad_candidates = new Meteor.Collection(null);  
 
-            for (var i = 0; i < persons.length; i++) {
-		    var tmpPer = {};
+            for (var i = 0; i < persons.length; i++) { 
+		var toBeAdded = true;
+
+		//teeme nõmedat filtreerimisjura, loodetavasti piisavalt optimiseeritult
+		//if you value your sanity, go away now. paras spagett. aga töötab. awwyeah.
+
+		filterLoop:
+		while (true) { 
+		    var query = Session.get("nimekirjad_candidates_search_query");
+		    if (query) { 
+			var filtersSet = false; //kas üldse mõni checkbox on valitud?
+
+			//täisnimi - nt "edgar a" > leiab üles "edgar ansip"
+			if (query.indexOf(" ") > 0 &&
+			    Session.get("nimekirjad_current_search_first_name_enabled") &&	
+			    Session.get("nimekirjad_current_search_last_name_enabled")) {
+			    
+				var fullname = (persons[i].firstName + " " + persons[i].lastName)
+						.trim().toLowerCase();
+				if (fullname.indexOf(query) >= 0) 				
+					break filterLoop;	
+			};
+
+
+			if (Session.get("nimekirjad_current_search_first_name_enabled")) {
+				filtersSet = true;
+				if (persons[i].firstName.trim().toLowerCase().indexOf(query) >= 0) 
+					break filterLoop;
+			};
+
+			if (Session.get("nimekirjad_current_search_last_name_enabled")) {
+				filtersSet = true;
+				if (persons[i].lastName.trim().toLowerCase().indexOf(query) >= 0) 
+					break filterLoop;
+			};
+
+			if (Session.get("nimekirjad_current_search_region_enabled")) {
+				filtersSet = true;
+				var regionName = getRegionNameById(persons[i].regionId)
+						 .trim().toLowerCase();
+				if (regionName.indexOf(query) >= 0) 
+					break filterLoop;
+			};
+
+			if (Session.get("nimekirjad_current_search_party_enabled")) {
+				filtersSet = true; 
+				var partyName = getPartyNameById(persons[i].partyId)
+						 .trim().toLowerCase();
+				if (partyName.indexOf(query) >= 0) 
+					break filterLoop;			
+			};
+			if (!filtersSet) break filterLoop; //ühtegi checkboxi pole valitud, näita kõiki
+			toBeAdded = false; //ei vastanud ühelegi filtrile.. 
+		      };
+		      
+		      break filterLoop;
+		    };
+		    if (!toBeAdded) continue; //skipime selle inimese, ei lisata
+
+		    var tmpPer = {}; //loome lisatava isiku objekti
 		    tmpPer.cid = persons[i].cid;
 		    tmpPer.firstName = persons[i].firstName;
 		    tmpPer.lastName = persons[i].lastName;
 		    tmpPer.partyId = persons[i].partyId; 
 		    tmpPer.regionName = getRegionNameById(persons[i].regionId);
 		    tmpPer.partyName = getPartyNameById(persons[i].partyId);
-		    if (persons[i].partyId == 0) tmpPer.noPartyLink = true; 
-	    
-		    //filterdame nime järgi
-	    
-		    var searched_name = Session.get("nimekirjad_candidates_search_query");
-		    if (searched_name) {
-		        //trim() võib olla..?
-		        var full_name = (tmpPer.firstName + " " + tmpPer.lastName)
-		            .trim()
-		            .toLowerCase();
-		        if (!full_name.indexOf(searched_name) == 0) continue;
-            		};
+		    if (persons[i].partyId == 0) tmpPer.noPartyLink = true;	    
     	
-    	        nimekirjad_candidates.insert(tmpPer);
+    	            nimekirjad_candidates.insert(tmpPer); //lisame
             };
 
 	    if (nimekirjad_candidates) 
@@ -80,7 +124,7 @@ Template.nimekirjad.currentSearchFirstNameEnabled = function() {
 };
 
 Template.nimekirjad.currentSearchLastNameEnabled = function() {
-	return Session.get("nimekirjad_current_search_first_last_enabled");
+	return Session.get("nimekirjad_current_search_last_name_enabled");
 };
 
 Template.nimekirjad.currentSearchRegionEnabled = function() {
