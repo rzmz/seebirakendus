2100ddffbadce560029251319eef6efaf1a1fd4b
Taavi Ilp <taaviilp@taaviilp-VirtualBox.(none)>
2013-03-20 00:00:43 +0200

    sain vist lõpuks sorteerimise tööle..

--- client/candidate_list.js
+++ client/candidate_list.js
@@ -1,47 +1,4 @@
-Session.set("candidates_list_loading", false);
 Session.set("nimekirjad_current_search_first_name_enabled", true);
-var nimekirjad_candidates;
-
-
-set_nimekirjad_candidates = function() {
-    // TODO - teha mingi ühine meetod kandidaadiinfo saamiseks	   
-	
-	//resetime listi
-
-    Session.set("candidates_list_loading", true);
-
-    if (!nimekirjad_candidates) nimekirjad_candidates = new Meteor.Collection(null);   
-    else nimekirjad_candidates.remove({});
-
-    var persons = Persons.find({}).fetch();
-
-        for (var i = 0; i < persons.length; i++) {
-            var tmpPer = {};
-            tmpPer.cid = persons[i].cid;
-            tmpPer.firstName = persons[i].firstName;
-            tmpPer.lastName = persons[i].lastName;
-	    tmpPer.partyId = persons[i].partyId; 
-            tmpPer.regionName = getRegionNameById(persons[i].regionId);
-            tmpPer.partyName = getPartyNameById(persons[i].partyId);
-	    if (persons[i].partyId == 0) tmpPer.noPartyLink = true; 
-    
-            //filterdame nime järgi
-    
-            var searched_name = Session.get("nimekirjad_candidates_search_query");
-            if (searched_name) {
-                //trim() võib olla..?
-                var full_name = (tmpPer.firstName + " " + tmpPer.lastName)
-                    .trim()
-                    .toLowerCase();
-                if (!full_name.indexOf(searched_name) == 0) continue;
-            };
-    
-            nimekirjad_candidates.insert(tmpPer);
-        };
-	
-	
-	Session.set("candidates_list_loading", false);
-};
 
 Template.nimekirjad.listLoading = function() {
 	return Session.get("candidates_list_loading");
@@ -50,12 +7,39 @@ Template.nimekirjad.listLoading = function() {
 Template.nimekirjad.candidates = function() {
 	//hack me baby one more time	
 
-	if (personsReady && regionsReady && partiesReady && nimekirjad_candidates) {		
-		return nimekirjad_candidates.find({}).fetch();
-	};
+	if (personsReady && regionsReady && partiesReady) {
+	     
+    	    var persons = Persons.find({}).fetch();
 
-	if (personsReady && regionsReady && partiesReady && !nimekirjad_candidates) {
-		set_nimekirjad_candidates();	
+	    nimekirjad_candidates = new Meteor.Collection(null);  
+
+            for (var i = 0; i < persons.length; i++) {
+		    var tmpPer = {};
+		    tmpPer.cid = persons[i].cid;
+		    tmpPer.firstName = persons[i].firstName;
+		    tmpPer.lastName = persons[i].lastName;
+		    tmpPer.partyId = persons[i].partyId; 
+		    tmpPer.regionName = getRegionNameById(persons[i].regionId);
+		    tmpPer.partyName = getPartyNameById(persons[i].partyId);
+		    if (persons[i].partyId == 0) tmpPer.noPartyLink = true; 
+	    
+		    //filterdame nime järgi
+	    
+		    var searched_name = Session.get("nimekirjad_candidates_search_query");
+		    if (searched_name) {
+		        //trim() võib olla..?
+		        var full_name = (tmpPer.firstName + " " + tmpPer.lastName)
+		            .trim()
+		            .toLowerCase();
+		        if (!full_name.indexOf(searched_name) == 0) continue;
+            		};
+    	
+    	        nimekirjad_candidates.insert(tmpPer);
+            };
+
+	    if (nimekirjad_candidates) 
+	        return nimekirjad_candidates.find({}).fetch();
+
 	};
 };
 
@@ -79,8 +63,6 @@ Template.nimekirjad.events({
 	    Session.set("nimekirjad_current_search_party_enabled", 
 		$('#party_checkbox').is(':checked'));	    
 	
-            set_nimekirjad_candidates();
-            //console.log(query);
             spinner.stop();
         }, 1000);
         //TODO - esimese asjana kustuta kõik väärtused..
